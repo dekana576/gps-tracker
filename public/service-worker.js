d6784d64bcb5a84f72c2282713b8058fdef9a228
dekana576 <dekana576@gmail.com>
2025-01-08 14:01:38 +0800

    berhenti tracking untuk save history

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('notificationclick', function (event) {
     event.notification.close();
     if (event.action === "stop") {
-        event.waitUntil(clients.openWindow('/')); // Ganti URL ini sesuai kebutuhan Anda
+        event.waitUntil(clients.openWindow('/save-history')); // Ganti URL ini sesuai kebutuhan Anda
     } else {
         event.waitUntil(clients.openWindow('/')); // Halaman utama
     }
